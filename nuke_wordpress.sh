4cd68ad527edd4ba38d09e03f2596c6bf93625f0
Brandon Taylor <brandon.taylor221@gmail.com>
2019-12-06 16:59:17 -0500

    fix

--- nuke_wordpress.sh
+++ nuke_wordpress.sh
@@ -1,4 +1,4 @@
-mysql --user root --password=Diddle22 < nuke_wordpress.sql
+mysql --user root --password=YOUR_PASSWORD_HERE < nuke_wordpress.sql
 # remove civicrm contents, and create a new writable folder for civicm
 rm --recursive --force /usr/share/wordpress/wp-content/uploads/civicrm
 mkdir /usr/share/wordpress/wp-content/uploads/civicrm
