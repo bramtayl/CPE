29d4525de77a5a2077938b6e27d79ff9518f8cef
Brandon Taylor <brandon.taylor221@gmail.com>
2020-03-05 12:38:59 -0500

    fix

--- nuke_wordpress.sh
+++
@@ -1,5 +0,0 @@
-mysql --user root --password=Diddle22 < nuke_wordpress.sql
-# remove civicrm contents, and create a new writable folder for civicm
-rm --recursive --force /usr/share/wordpress/wp-content/uploads/civicrm
-mkdir /usr/share/wordpress/wp-content/uploads/civicrm
-chmod a+rwx /usr/share/wordpress/wp-content/uploads/civicrm
--- nuke_wordpress.sql
+++
@@ -1,4 +0,0 @@
--- remove the wordpress database, create a new one, and give the wordpress user access
-drop database wordpress;
-create database wordpress;
-grant all on wordpress.* to wordpress@localhost;
